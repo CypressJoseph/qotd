5e7ce565a526ebe458d8cbe232e15c1ff3b7d501
Joseph Weissman <joseph@cypress.io>
2020-03-19 14:53:48 -0400

    Updates

---
+++ precache-manifest.72221d4ce4e593fb4b80f5eeed161739.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc85271e270ed22489b1a55b536017ed",
+    "url": "/qotd/index.html"
+  },
+  {
+    "revision": "0b66986e700ad9e723af",
+    "url": "/qotd/static/css/main.b1f47a09.chunk.css"
+  },
+  {
+    "revision": "fbdceb66876b8f4ef899",
+    "url": "/qotd/static/js/2.0eea295a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/qotd/static/js/2.0eea295a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b66986e700ad9e723af",
+    "url": "/qotd/static/js/main.88c5d529.chunk.js"
+  },
+  {
+    "revision": "e2bf5d7e61d37cfe3afa",
+    "url": "/qotd/static/js/runtime-main.1e3f32e7.js"
+  }
+]);
