72afb563e8efae6ee70d465392a475258bb733da
Joseph Weissman <joseph@cypress.io>
2020-03-26 15:59:13 -0400

    explicitly wait for qotd api

--- cypress/integration/features/qotd-button.spec.ts
+++ cypress/integration/features/qotd-button.spec.ts
@@ -2,8 +2,18 @@
 
 context('Given I am on the homepage', () => {
   beforeEach(() => {
+    cy.server()
+    cy.route(
+      "**/qod*",
+      { contents: {quotes: [{
+        quote: "A pitcher's got to be good and he's got to be lucky to get a no hit game",
+        author: "Cy Young",
+      }]}}
+    ).as('getQuote')
     cy.visit('') // go home
     cy.get('body').trigger('keydown', { key: 'Enter', force: true })
+    // i think we need to stub the quote...
+    cy
   })
 
   describe('When I click the button', () => {
@@ -12,7 +22,7 @@ context('Given I am on the homepage', () => {
           let inspireMe = cy.get('.LoadQuoteButton')
           inspireMe.should('exist')
           inspireMe.click()
-          cy.get('.Quote').should('exist')
+          cy.wait("@getQuote").then(() => cy.get('.Quote').should('exist'))
       })
   })
 });
