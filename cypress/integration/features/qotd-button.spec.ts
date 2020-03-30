9456de318b705274d6298bf65c9ce7e33d073d37
Joseph Weissman <joseph@cypress.io>
2020-03-30 17:02:15 -0400

    re-enable specs -bump up timer on ci

--- cypress/integration/features/qotd-button.spec.ts
+++ cypress/integration/features/qotd-button.spec.ts
@@ -4,16 +4,14 @@ context('Given I am on the homepage', () => {
   beforeEach(() => {
     cy.server()
     cy.route(
-      "**/qod*",
+      "https://quotes.rest/qod/*",
       { contents: {quotes: [{
         quote: "A pitcher's got to be good and he's got to be lucky to get a no hit game",
         author: "Cy Young",
       }]}}
     ).as('getQuote')
     cy.visit('') // go home
-    cy.get('body').trigger('keydown', { key: 'Enter', force: true })
-    // i think we need to stub the quote...
-    cy
+    // cy.get('body').trigger('keydown', { key: 'Enter', force: true })
   })
 
   describe('When I click the button', () => {
